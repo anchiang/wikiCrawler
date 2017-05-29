5d2505d11f05d0ea69b0f29d8076e0ce4eceba35
anchiang <anchiang@gmail.com>
2017-05-29 10:36:38 +0800

    微調 nurl

--- wikiCrawler_tutorial3.py
+++ wikiCrawler_tutorial3.py
@@ -5,22 +5,21 @@ import urllib.parse
 from bs4 import BeautifulSoup as bs
 import re
 
-def get_new_urls(new_url): #取得網頁內的超連結
-    response = urllib.request.urlopen(new_url)
+def get_new_urls(nurl):
+    response = urllib.request.urlopen(nurl)
     html_cont = response.read()
     soup = bs(html_cont,'html.parser',from_encoding='utf-8')
     urls = soup.find_all('a',href=re.compile("^/wiki/\w+"))
-    for url in urls[:5]:
-        if not re.search("\w+:\w",url['href']):
-            full_url = urllib.parse.urljoin(new_url,url[
-                    'href'])
-            new_urls.append(full_url) 
+    for url in urls[:10]:
+        if not re.search("\w:\w", url['href']):
+            furl = urllib.parse.urljoin(nurl,url['href'])
+            new_urls.append(furl)
 
 URL = "https://en.wikipedia.org/wiki/The_Matrix"
 new_urls=[URL]
 count = 0
 while len(new_urls)>0 and count <10:
-    count +=1
+    count += 1
     new_url = new_urls.pop()
-    print(count,new_url)
+    print(count, new_url)
     get_new_urls(new_url)
